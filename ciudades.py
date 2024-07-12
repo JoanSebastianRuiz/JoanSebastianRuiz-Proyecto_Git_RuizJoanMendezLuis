9f0561ac510854dfe41d0e8960e4cb65e1c57b1e
JoanSebastianRuiz <joansebastianruizangarita10@gmail.com>
2024-07-12 15:57:57 -0500

    fix: :bug: Correccion funcion editar ciudad

--- ciudades.py
+++ ciudades.py
@@ -20,15 +20,15 @@ def editar_ciudad(datos):
     ciudad = input("Ingrese el nombre de la ciudad: ")
     for i in range(len(datos["ciudades"])):
         if datos["ciudades"][i]["nombre"] == ciudad:
-            ciudad["nombre"]=input("Ingrese el nombre: ")
-    ciudad["postal"]=input("Ingrese el código postal: ")
+            datos["ciudades"][i]["nombre"]=input("Ingrese el nombre: ")
+    datos["ciudades"][i]["postal"]=input("Ingrese el código postal: ")
     try:
-        ciudad["poblacion"]=int(input("Ingrese la población estimada: "))
+        datos["ciudades"][i]["poblacion"]=int(input("Ingrese la población estimada: "))
     except Exception:
         print("¡Valor ingresado no válido!")
         print("Se debe actualizar luego con la opción (2) 'Editar las ciudades existentes'")
-        ciudad["poblacion"]=0
-    ciudad["pais"]=input("Ingrese el País: ")
+        datos["ciudades"][i]["poblacion"]=0
+    datos["ciudades"][i]["pais"]=input("Ingrese el País: ")
             
     print("Ciudad actualizada con éxito!")
     return datos
